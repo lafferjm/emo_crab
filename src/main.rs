ec41a0854d72ad31728285458b01599f74f25453
Justin Lafferty <justin.lafferty@kw.com>
2025-03-06 18:50:47 -0500

    initialize values

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+struct Chip8 {
+    registers: [u8; 16],
+    memory: [u8; 4096],
+    index: u8,
+    pc: u16,
+    stack: [u8; 16],
+    sp: u8,
+    delay_timer: u8,
+    sound_timer: u8,
+    keypad: [u8; 16],
+    video: [u32; 64 * 32],
+    opcode: u16,
+}
+
+impl Chip8 {
+    pub fn new() -> Self {
+        Self {
+            registers: [0x0; 16],
+            memory: [0x0; 4096],
+            index: 0x0,
+            pc: 0x200,
+            stack: [0x0; 16],
+            sp: 0x0,
+            delay_timer: 0x0,
+            sound_timer: 0x0,
+            keypad: [0x0; 16],
+            video: [0x0; 64 * 32],
+            opcode: 0x0,
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+    let chip8 = Chip8::new();
 }
