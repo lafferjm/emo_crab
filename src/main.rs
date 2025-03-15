9047fd65963426bbd9c1cb20a27ef99047185ab4
Justin Lafferty <justin.lafferty@kw.com>
2025-03-15 15:58:20 -0400

    fix incorrect commands

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,21 @@ use macroquad::window::{Conf, clear_background, next_frame};
 use macroquad::{color, shapes};
 
 struct Chip8 {
-    registers: [u16; 16],
-    memory: [u16; 4096],
+    registers: [u8; 16],
+    memory: [u8; 4096],
     index: u16,
     pc: u16,
     stack: [u16; 16],
-    sp: u16,
-    delay_timer: u16,
-    sound_timer: u16,
-    keypad: [u16; 16],
-    video: [u32; 64 * 32],
+    sp: u8,
+    delay_timer: u8,
+    sound_timer: u8,
+    keypad: [u8; 16],
+    video: [u8; 64 * 32],
 }
 
 impl Chip8 {
     pub fn new() -> Self {
-        // stolen from varius chip 8 emulators
-        let font: [u16; 80] = [
+        let font: [u8; 80] = [
             0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
             0x20, 0x60, 0x20, 0x20, 0x70, // 1
             0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
@@ -40,9 +39,9 @@ impl Chip8 {
             0xF0, 0x80, 0xF0, 0x80, 0x80, // F
         ];
 
-        let mut memory: [u16; 4096] = [0x0; 4096];
-        for (i, value) in font.iter().enumerate() {
-            memory[0x50 + i] = *value;
+        let mut memory: [u8; 4096] = [0x0; 4096];
+        for (i, &value) in font.iter().enumerate() {
+            memory[0x50 + i] = value;
         }
 
         Self {
@@ -50,7 +49,6 @@ impl Chip8 {
             memory,
             index: 0x0,
             pc: 0x200,
-            // Maybe I can replace stack and sp with a vector, future test
             stack: [0x0; 16],
             sp: 0x0,
             delay_timer: 0x0,
@@ -65,8 +63,8 @@ impl Chip8 {
         let mut contents = vec![];
         file.read_to_end(&mut contents)?;
 
-        for (i, value) in contents.iter().enumerate() {
-            self.memory[0x200 + i] = *value as u16;
+        for (i, &value) in contents.iter().enumerate() {
+            self.memory[0x200 + i] = value;
         }
 
         Ok(())
@@ -74,23 +72,18 @@ impl Chip8 {
 
     pub fn get_instruction(&mut self) -> u16 {
         let instruction =
-            (self.memory[self.pc as usize] << 8) | self.memory[(self.pc + 1) as usize];
-
+            (self.memory[self.pc as usize] as u16) << 8 | self.memory[self.pc as usize + 1] as u16;
         self.pc += 2;
-
         instruction
     }
 
     pub fn decode_instruction(&mut self, instruction: u16) {
         match (instruction & 0xF000) >> 12 {
             0x0 => {
-                // Clear screen
                 if instruction & 0x00FF == 0xE0 {
                     self.video = [0x0; 64 * 32];
                 }
-
-                // return from subroutine
-                if instruction & 0x0FF == 0xEE {
+                if instruction & 0x00FF == 0xEE {
                     self.sp -= 1;
                     self.pc = self.stack[self.sp as usize];
                 }
@@ -131,45 +124,39 @@ impl Chip8 {
     }
 
     fn set_register(&mut self, instruction: u16) {
-        let register = (instruction & 0x0F00) >> 8;
-        let value = instruction & 0x00FF;
-
-        self.registers[register as usize] = value;
+        let register = ((instruction & 0x0F00) >> 8) as usize;
+        let value = (instruction & 0x00FF) as u8;
+        self.registers[register] = value;
     }
 
     fn add_to_register(&mut self, instruction: u16) {
-        let register = (instruction & 0x0F00) >> 8;
-        let value = instruction & 0x00FF;
-
-        self.registers[register as usize] += value;
+        let register = ((instruction & 0x0F00) >> 8) as usize;
+        let value = (instruction & 0x00FF) as u8;
+        self.registers[register] = self.registers[register].wrapping_add(value);
     }
 
     fn set_index_register(&mut self, instruction: u16) {
-        let value = instruction & 0x0FFF;
-        self.index = value;
+        self.index = instruction & 0x0FFF;
     }
 
     fn draw(&mut self, instruction: u16) {
-        let x = (instruction & 0x0F00) >> 8;
-        let x = self.registers[x as usize] % 64;
+        let x = ((instruction & 0x0F00) >> 8) as usize;
+        let x = self.registers[x] % 64;
 
-        let y = (instruction & 0x00F0) >> 4;
-        let y = self.registers[y as usize] % 32;
+        let y = ((instruction & 0x00F0) >> 4) as usize;
+        let y = self.registers[y] % 32;
 
-        let height = instruction & 0x000F;
+        let height = (instruction & 0x000F) as u16;
 
         self.registers[0xF] = 0;
 
         for row in 0..height {
             let sprite = self.memory[(self.index + row) as usize];
-
             for column in 0..8 {
-                // 0x80 == 1000_0000 we are checking each value by shifting each time
                 let pixel = sprite & (0x80 >> column);
-
                 if pixel != 0 {
                     let x_pos = (x + column) % 64;
-                    let y_pos = (y + row) % 32;
+                    let y_pos = (y + row as u8) % 32;
                     let index = y_pos as usize * 64 + x_pos as usize;
 
                     if self.video[index] == 1 {
@@ -184,72 +171,64 @@ impl Chip8 {
     fn call_subroutine(&mut self, instruction: u16) {
         let location = instruction & 0x0FFF;
         self.stack[self.sp as usize] = self.pc;
-        self.sp += 1;
+        self.sp = self.sp.wrapping_add(1);
         self.pc = location;
     }
 
     fn skip_if_equal(&mut self, instruction: u16) {
-        let register = (instruction & 0x0F00) >> 8;
-        let value = instruction & 0x00FF;
-
-        if self.registers[register as usize] == value {
+        let register = ((instruction & 0x0F00) >> 8) as usize;
+        let value = (instruction & 0x00FF) as u8;
+        if self.registers[register] == value {
             self.pc += 2;
         }
     }
 
     fn skip_if_not_equal(&mut self, instruction: u16) {
-        let register = (instruction & 0x0F00) >> 8;
-        let value = instruction & 0x00FF;
-
-        if self.registers[register as usize] != value {
+        let register = ((instruction & 0x0F00) >> 8) as usize;
+        let value = (instruction & 0x00FF) as u8;
+        if self.registers[register] != value {
             self.pc += 2;
         }
     }
 
     fn skip_if_registers_equal(&mut self, instruction: u16) {
-        let x_register = (instruction & 0x0F00) >> 8;
-        let y_register = (instruction & 0x00F0) >> 4;
-
-        if self.registers[x_register as usize] == self.registers[y_register as usize] {
+        let x_register = ((instruction & 0x0F00) >> 8) as usize;
+        let y_register = ((instruction & 0x00F0) >> 4) as usize;
+        if self.registers[x_register] == self.registers[y_register] {
             self.pc += 2;
         }
     }
 
     fn skip_if_registers_not_equal(&mut self, instruction: u16) {
-        let x_register = (instruction & 0x0F00) >> 8;
-        let y_register = (instruction & 0x00F0) >> 4;
-
-        if self.registers[x_register as usize] != self.registers[y_register as usize] {
+        let x_register = ((instruction & 0x0F00) >> 8) as usize;
+        let y_register = ((instruction & 0x00F0) >> 4) as usize;
+        if self.registers[x_register] != self.registers[y_register] {
             self.pc += 2;
         }
     }
 
     fn set_x_register(&mut self, instruction: u16) {
-        let x_register = (instruction & 0x0F00) >> 8;
-        let y_register = (instruction & 0x00F0) >> 4;
-
-        self.registers[x_register as usize] = self.registers[y_register as usize];
+        let x_register = ((instruction & 0x0F00) >> 8) as usize;
+        let y_register = ((instruction & 0x00F0) >> 4) as usize;
+        self.registers[x_register] = self.registers[y_register];
     }
 
     fn binary_or(&mut self, instruction: u16) {
-        let x_register = (instruction & 0x0F00) >> 8;
-        let y_register = (instruction & 0x00F0) >> 4;
-
-        self.registers[x_register as usize] |= self.registers[y_register as usize];
+        let x_register = ((instruction & 0x0F00) >> 8) as usize;
+        let y_register = ((instruction & 0x00F0) >> 4) as usize;
+        self.registers[x_register] |= self.registers[y_register];
     }
 
     fn binary_and(&mut self, instruction: u16) {
-        let x_register = (instruction & 0x0F00) >> 8;
-        let y_register = (instruction & 0x00F0) >> 4;
-
-        self.registers[x_register as usize] &= self.registers[y_register as usize];
+        let x_register = ((instruction & 0x0F00) >> 8) as usize;
+        let y_register = ((instruction & 0x00F0) >> 4) as usize;
+        self.registers[x_register] &= self.registers[y_register];
     }
 
     fn logical_xor(&mut self, instruction: u16) {
-        let x_register = (instruction & 0x0F00) >> 8;
-        let y_register = (instruction & 0x00F0) >> 4;
-
-        self.registers[x_register as usize] ^= self.registers[y_register as usize];
+        let x_register = ((instruction & 0x0F00) >> 8) as usize;
+        let y_register = ((instruction & 0x00F0) >> 4) as usize;
+        self.registers[x_register] ^= self.registers[y_register];
     }
 }
 
